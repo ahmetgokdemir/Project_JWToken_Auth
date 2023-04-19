1536438f1c3966789b33f080360908fed6c748e1
ahmetgokdemir <ahmetgokdemirtc@gmail.com>
2023-04-19 16:03:11 +0300

    lambda syntax function

--- UdemyAuthServer.Service/ObjectMapper.cs
+++ UdemyAuthServer.Service/ObjectMapper.cs
@@ -7,8 +7,11 @@ namespace UdemyAuthServer.Service
 {
     public static class ObjectMapper
     {
+        // içerideki bir fonksiyondur (lambda syntax)... () => demek, fonks. parametre almıyor demek ve geriye de config.CreateMapper() dönüyor yani IMapper interface'ini.. 
         private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
         {
+            // config, IMapper'ı oluşturur (CreateMapper metodu ile) 
+            // içerideki bir fonksiyondur (lambda syntax)... cfg => demek, fonks. parametre almış ve IMapperConfigurationExpression tipinde 
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile<DtoMapper>();
